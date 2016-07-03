96020e520c2af553fd9d0d4f8af6131c1a586f5d
Iris Sakura <sgjsakura@hotmail.com>
2016-07-04 01:21:30 +0800

    Add new Link Generators

---
+++ Sakura.AspNetCore.Extensions/src/Sakura.AspNetCore.Mvc.PagedList/Generators/SimpleLinkGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Sakura.AspNetCore.Mvc.Internal;
+
+namespace Sakura.AspNetCore.Mvc.Generators
+{
+	/// <summary>
+	///     Generate a link url from a simple string.
+	/// </summary>
+	public class SimpleLinkGenerator : IPagerItemLinkGenerator
+	{
+		/// <summary>
+		///     Initialize a new generator with specified information.
+		/// </summary>
+		/// <param name="text">The text content.</param>
+		/// <exception cref="ArgumentNullException"><paramref name="text" /> is <c>null</c>.</exception>
+		public SimpleLinkGenerator([NotNull] string text)
+		{
+			if (text == null)
+			{
+				throw new ArgumentNullException(nameof(text));
+			}
+
+			Text = text;
+		}
+
+		/// <summary>
+		///     Get the text content.
+		/// </summary>
+		[PublicAPI]
+		[NotNull]
+		public string Text { get; }
+
+		#region Implementation of IPagerItemLinkGenerator
+
+		/// <summary>
+		///     Generate the link url for the specified <see cref="PagerItem" />.
+		/// </summary>
+		/// <param name="context">The generation context.</param>
+		public string GenerateLink(PagerItemGenerationContext context) => Text;
+
+		#endregion
+	}
+}
