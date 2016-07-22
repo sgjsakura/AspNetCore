3764a32550eb4940baf88ccd456d91a347c58f56
Iris Sakura <sgjsakura@hotmail.com>
2016-07-22 16:45:07 +0800

    Update TagHelpers library to 1.2.0

---
+++ Sakura.AspNetCore.Extensions/src/Sakura.AspNetCore.Mvc.TagHelpers/IdFormatTagHelper.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Sakura.AspNetCore.Mvc.TagHelpers
+{
+	/// <summary>
+	/// A tag helper used to generate 
+	/// </summary>
+	[HtmlTargetElement("*", Attributes = IdFormatAttributeName)]
+	public class IdFormatTagHelper : TagHelper
+	{
+		/// <summary>
+		/// Get the attribute name used for <see cref="IdFormat"/> property. This field is constant.
+		/// </summary>
+		public const string IdFormatAttributeName = "asp-id-format";
+
+		/// <summary>
+		/// Get or set the format string for the id.
+		/// </summary>
+		[HtmlAttributeName(IdFormatAttributeName)]
+		public string IdFormat { get; set; }
+
+		/// <summary>
+		/// Get or set the start value for each format string.
+		/// </summary>
+		public static int IdStart { get; set; } = 1;
+
+		/// <summary>
+		/// Get or set the key used to store the id-format counting information.
+		/// </summary>
+		public static string ViewDataKey { get; set; } = "ASP_IdFormatCountDictionary";
+
+		[ViewContext]
+		[HtmlAttributeNotBound]
+		public ViewContext ViewContext { get; set; }
+
+		/// <summary>
+		/// Get the dictionary stored the id format counting inforamtion.
+		/// </summary>
+		protected ConcurrentDictionary<string, int> IdFormatCountDictionary
+		{
+			get
+			{
+				if (ViewDataKey == null)
+				{
+					throw new InvalidOperationException($"The property of '{nameof(ViewDataKey)}' cannot be null.");
+				}
+
+				object result;
+
+				if (ViewContext.ViewData.TryGetValue(ViewDataKey, out result) && result is ConcurrentDictionary<string, int>)
+				{
+					return (ConcurrentDictionary<string, int>)result;
+				}
+
+				var newDic = new ConcurrentDictionary<string, int>(StringComparer.Ordinal);
+				ViewContext.ViewData[ViewDataKey] = newDic;
+				return newDic;
+			}
+		}
+
+		/// <summary>
+		/// Get next count for current id format.
+		/// </summary>
+		/// <returns>The next count for current id format.</returns>
+		protected int GetNextCount()
+		{
+			if (string.IsNullOrEmpty(IdFormat))
+			{
+				throw new InvalidOperationException($"The value of html attribute '{IdFormatAttributeName}' cannot be null.");
+			}
+
+			return IdFormatCountDictionary.AddOrUpdate(IdFormat, IdStart, (key, value) => value + 1);
+		}
+
+		/// <summary>
+		/// Synchronously executes the <see cref="TagHelper" /> with the given <paramref name="context" /> and
+		/// <paramref name="output" />.
+		/// </summary>
+		/// <param name="context">Contains information associated with the current HTML tag.</param>
+		/// <param name="output">A stateful HTML element used to generate an HTML tag.</param>
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			if (string.IsNullOrEmpty(IdFormat))
+			{
+				throw new InvalidOperationException($"The value of html attribute '{IdFormatAttributeName}' cannot be null.");
+			}
+
+			if (context.AllAttributes.ContainsName("id"))
+			{
+				throw new InvalidOperationException($"You cannot specify both 'id' and '{IdFormatAttributeName}' attribute on one element.");
+			}
+
+			output.Attributes.SetAttribute("id", string.Format(CultureInfo.InvariantCulture, IdFormat, GetNextCount()));
+		}
+	}
+}
