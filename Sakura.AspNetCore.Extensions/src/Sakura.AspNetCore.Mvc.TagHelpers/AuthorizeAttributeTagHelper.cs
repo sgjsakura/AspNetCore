c842eb8be34801a9131d1613c5d95d357654eadc
Iris Sakura <sgjsakura@hotmail.com>
2017-01-03 21:35:49 +0800

    Update TagHelper to 1.3.0

---
+++ Sakura.AspNetCore.Extensions/src/Sakura.AspNetCore.Mvc.TagHelpers/AuthorizeAttributeTagHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Sakura.AspNetCore.Mvc.TagHelpers
+{
+	/// <summary>
+	/// Provide authorization-based HTML generation control on existing HTML elements.
+	/// </summary>
+	[HtmlTargetElement("*", Attributes = PolicyAttributeName)]
+	public class AuthorizeAttributeTagHelper : AuthorizeTagHelperBase
+	{
+		#region Tag Helper Constants
+
+		/// <summary>
+		/// Get the HTML attribute name associated with <see cref="Policy"/> property. This field is constant.
+		/// </summary>
+		[PublicAPI]
+		public const string PolicyAttributeName = "asp-authorize-policy";
+
+		/// <summary>
+		/// Get the HTML attribute name associated with <see cref="Resource"/> property. This field is constant.
+		/// </summary>
+		[PublicAPI]
+		public const string ResourceAttributeName = "asp-authorize-resource";
+
+		#endregion
+
+		/// <summary>
+		/// Initialize a new instance of <see cref="AuthorizeTagHelper"/>.
+		/// </summary>
+		/// <param name="authorizationService">The <see cref="IAuthorizationService"/> instance.</param>
+		[UsedImplicitly]
+		public AuthorizeAttributeTagHelper(IAuthorizationService authorizationService)
+			: base(authorizationService)
+		{
+		}
+
+		/// <summary>
+		/// Get or set the policy name which should be melt. 
+		/// </summary>
+		[HtmlAttributeName(PolicyAttributeName)]
+		[UsedImplicitly(ImplicitUseKindFlags.Assign)]
+		public override string Policy { get; set; }
+
+		/// <summary>
+		/// Get or set the additional resource used to authoirzation check if necessary.
+		/// </summary>
+		/// <seealso cref="IAuthorizationService.AuthorizeAsync(ClaimsPrincipal, object, string)"/>
+		[HtmlAttributeName(ResourceAttributeName)]
+		[UsedImplicitly(ImplicitUseKindFlags.Assign)]
+		public override object Resource { get; set; }
+	}
+}
---
+++ Sakura.AspNetCore.Extensions/src/Sakura.AspNetCore.Mvc.TagHelpers/AuthorizeTagHelperBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Sakura.AspNetCore.Mvc.TagHelpers
+{
+	/// <summary>
+	/// Provide common implementations for both <see cref="AuthorizeTagHelper"/> and <see cref="AuthorizeAttributeTagHelper"/>.
+	/// </summary>
+	public abstract class AuthorizeTagHelperBase : TagHelper
+	{
+		/// <summary>
+		/// Initialize a new instance of <see cref="AuthorizeTagHelperBase"/>.
+		/// </summary>
+		/// <param name="authorizationService">The <see cref="IAuthorizationService"/> instance.</param>
+		protected AuthorizeTagHelperBase(IAuthorizationService authorizationService)
+		{
+			AuthorizationService = authorizationService;
+		}
+
+		private IAuthorizationService AuthorizationService { get; }
+
+		/// <summary>
+		/// Get the authoization policy name which current user must melt in order to see the content.
+		/// </summary>
+		public abstract string Policy { get; set; }
+
+		/// <summary>
+		/// Get or set the additional resource used to authoirzation check if necessary.
+		/// </summary>
+		/// <seealso cref="IAuthorizationService.AuthorizeAsync(ClaimsPrincipal, object, string)"/>
+		public abstract object Resource { get; set; }
+
+		/// <summary>
+		/// Get or set the view context for this tag helper.
+		/// </summary>
+		[HtmlAttributeNotBound]
+		[ViewContext]
+		[UsedImplicitly(ImplicitUseKindFlags.Assign)]
+		public ViewContext ViewContext { get; set; }
+
+		#region Overrides of TagHelper
+
+		/// <inheritdoc />
+		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			// Omit the start and end tag of itself.
+			output.TagName = null;
+
+			// If authorization fails, do not output anything.
+			if (!await AuthorizationService.AuthorizeAsync(ViewContext.HttpContext.User, Resource, Policy))
+			{
+				output.SuppressOutput();
+			}
+		}
+
+		#endregion
+	}
+}
