9519978b941a745bdda328f2e576ce331cdd85ef
Iris Sakura <sgjsakura@hotmail.com>
2017-05-17 17:24:00 +0800

    Add service collection extensions

---
+++ Sakura.AspNetCore.Extensions/Sakura.AspNetCore.DynamicLocalizer/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Sakura.AspNetCore.Localization
+{
+	/// <summary>
+	/// Provide extension methods to add services. This class is static.
+	/// </summary>
+    public static class ServiceCollectionExtensions
+    {
+		/// <summary>
+		/// Add default implementation for <see cref="IDynamicLocallizer"/> service.
+		/// </summary>
+		/// <param name="services">The service collection container.</param>
+	    public static void AddDynamicLocalizer([NotNull] this ServiceCollection services)
+	    {
+		    if (services == null)
+		    {
+			    throw new ArgumentNullException(nameof(services));
+		    }
+
+			services.TryAddTransient<IDynamicLocallizer, DynamicLocalizer>();
+	    }
+    }
+}
